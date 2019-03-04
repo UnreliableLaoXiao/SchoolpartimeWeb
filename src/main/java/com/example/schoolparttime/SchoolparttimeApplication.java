0dc4ef72850f2b50690a26750faf57648005694e
Xiaohei <heyxhazx1003@163.com>
2019-03-04 20:52:21 +0800

    初始化服务器代码

---
+++ src/main/java/com/example/schoolparttime/SchoolparttimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolparttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolparttimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolparttimeApplication.class, args);
+    }
+
+}
